928717b87fa35e06f1dc042a892bfaaeea352246
maksim <maximum38@ya.ru>
2021-03-27 19:20:04 +0300

    first commit named pizdec

---
+++ testgit.java
@@ -0,0 +1,13 @@
+/* Name it "Hello.java" 
+ * Compile it by running "javac Hello.java"
+ * Run it by typing "java Hello"
+ */
+
+class testgit {
+	public static void main(String args[])
+	{
+    	System.out.println("Hello, git!");
+	}
+}
+
+
