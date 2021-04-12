4cc5d8b354555e17a97f2b04843107de35ec8691
maksim <maximum38@ya.ru>
2021-04-12 21:05:17 +0300

    3

---
+++ home_work_3/calcs/simple/CalculatorWithMathCopy.java
@@ -0,0 +1,37 @@
+package home_work_3.calcs.simple;
+
+public class CalculatorWithMathCopy {
+    public static void main(String[] args) {
+
+    }
+    public double divide(double a, double b){
+        return a/b;
+    }
+    public double mult(double a, double b){
+        return a*b;
+    }
+
+    public double subt(double a, double b){
+
+        return 0;
+    }
+    public double addition(double a, double b){
+
+        return a+b;
+    }
+
+    public double pow(double a, double b){
+        return Math.pow(a,b);
+    }
+
+    public double abs(double a){
+        return Math.abs(a);
+    }
+
+    public double root(double a){
+        return Math.sqrt(a);
+    }
+    public double cbrt(double a){
+        return Math.cbrt(a);
+    }
+}
